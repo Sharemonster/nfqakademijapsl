db9887f0d357904130c75922ac2f63e28ffa1c5f
Sharemonster <m.jurkynas@gmail.com>
2017-09-16 23:51:11 +0300

    It Works?

--- index.php
+++ index.php
@@ -13,7 +13,6 @@ Licence URI: http://www.os-templates.com/template-terms
 <link rel="stylesheet" href="layout/styles/layout.css" type="text/css" />
 </head>
 <body id="top">
-
 <!-- ####################################################################################################### -->
 <div class="wrapper col2">
   <div id="topbar">
@@ -27,22 +26,6 @@ Licence URI: http://www.os-templates.com/template-terms
   </div>
 </div>
 <!-- ####################################################################################################### -->
-<div class="wrapper col3">
-  <div id="waterwheelCarousel">
-    <img src="images/carousel/1.gif" alt="" />
-    <img src="images/carousel/2.gif" alt="" />
-    <img src="images/carousel/3.gif" alt="" />
-    <img src="images/carousel/4.gif" alt="" />
-    <img src="images/carousel/5.gif" alt="" />
-    <img src="images/carousel/6.gif" alt="" />
-    <img src="images/carousel/7.gif" alt="" />
-    <img src="images/carousel/8.gif" alt="" />
-    <img src="images/carousel/9.gif" alt="" />
-    <img src="images/carousel/10.gif" alt="" />
-    <img src="images/carousel/11.gif" alt="" />
-  </div>
-</div>
-<!-- ####################################################################################################### -->
 <div class="wrapper col4">
   <div id="featured_intro">
     <div class="fl_left">
@@ -52,55 +35,6 @@ Licence URI: http://www.os-templates.com/template-terms
       <h2>About This Template !</h2>
       <p>This is a W3C standards compliant free website template from <a href="http://www.os-templates.com/">OS Templates</a>. This template is distributed using a <a href="http://www.os-templates.com/template-terms">Website Template Licence</a>, which allows you to use and modify the template for both personal and commercial use when you keep the provided credit links in the footer. For more CSS templates visit <a href="http://www.os-templates.com/">Free Website Templates</a>.</p>
       <p class="readmore"><a href="#">Continue Reading &raquo;</a></p>
-    </div>
-    <br class="clear" />
-  </div>
-</div>
-<!-- ####################################################################################################### -->
-<div class="wrapper col5">
-  <div id="footer">
-    <div class="footbox twitter">
-      <h2>Latest From Twitter</h2>
-      <ul>
-        <li><a href="#">@TwitterName</a> nibhpede faucibulum ris phasellentum elit sed hend quisque.  1 day ago</li>
-        <li><a href="#">@TwitterName</a> tellentumst monterdum augue curabitae elit felit, elitligula adipiscingilla. 1 day ago</li>
-        <li><a href="#">@TwitterName</a> libero praesentum sed? 1 day ago</li>
-        <li><a href="#">@TwitterName</a> condimentesque nec facilis pretium tellenterdum. Vivamusid elit curabitur augue. 1 day ago</li>
-        <li><a href="#">@TwitterName</a> ligula sed ipsum in tellentesque donec ac. 1 day ago</li>
-      </ul>
-    </div>
-    <div class="footbox flickr">
-      <h2>Latest From Flickr</h2>
-      <ul>
-        <li><a href="#"><img src="images/demo/80x80.gif" alt="" /></a></li>
-        <li><a href="#"><img src="images/demo/80x80.gif" alt="" /></a></li>
-        <li><a href="#"><img src="images/demo/80x80.gif" alt="" /></a></li>
-        <li><a href="#"><img src="images/demo/80x80.gif" alt="" /></a></li>
-        <li><a href="#"><img src="images/demo/80x80.gif" alt="" /></a></li>
-        <li><a href="#"><img src="images/demo/80x80.gif" alt="" /></a></li>
-      </ul>
-      <br class="clear" />
-    </div>
-    <div class="footbox posts">
-      <h2>Latest Blog Posts</h2>
-      <ul>
-        <li><a href="#">Lorem ipsum dolor</a></li>
-        <li><a href="#">Lorem ipsum dolor sit amet</a></li>
-        <li><a href="#">Praesent et eros</a></li>
-        <li><a href="#">Praesent et eros</a></li>
-        <li><a href="#">Lorem ipsum dolor</a></li>
-        <li><a href="#">Suspendisse in neque</a></li>
-        <li><a href="#">Phasellus tempor vestibulum</a></li>
-        <li><a href="#">Vestibulum quis augu mauris</a></li>
-        <li class="last"><a href="#">Suspendisse in neque</a></li>
-      </ul>
-    </div>
-    <div class="footbox banners last">
-      <h2>Advertise Here</h2>
-      <ul>
-        <li><a href="#"><img src="images/demo/200x150.gif" alt="" /></a></li>
-        <li><a href="#"><img src="images/demo/200x150.gif" alt="" /></a></li>
-      </ul>
     </div>
     <br class="clear" />
   </div>
