6ea5269dc153992a14d8703e63bef309dc4a44d6
Sharemonster <m.jurkynas@gmail.com>
2017-09-17 20:55:12 +0300

    Hope it works

--- index.php
+++ index.php
@@ -1,23 +1,46 @@
 <!DOCTYPE html >
 
 <head>
-<title>Touch of Purple</title>
+<title>Pirmas puslapis</title>
 <meta content="text/html; charset=iso-8859-1" />
-<link rel="stylesheet" href="layout/styles/layout.css" type="text/css" />
+<link rel="stylesheet" href="css.css" type="text/css" />
 </head>
 
-<body id="top">
-<div class="wrapper col2">
-  <div id="topbar">
-    <div id="topnav">
-      <ul>
-        <li class="active"><a href="index.php">Home</a></li>
-        <li><a href="style-demo.php">Style Demo</a></li>
-      </ul>
-    </div>
-    <br class="clear" />
+<body>
+  <div class="center">
+    <img src="images/product.png"/>
   </div>
-</div>
+
+  <p>Component Order Form</p>
+
+  <FORM ACTION="apdoroti.php" method=post>
+    <table border=0>
+      <tr>
+        <td width=150>Item<br>
+        &nbsp;</td>
+        <td width=15>Quantity<br>
+        &nbsp;</td>
+      </tr>
+      <tr>
+        <td>Bases</td>
+        <td align="center"><input type="text" name="qtybases" size="3" maxlength="3"></td>
+      </tr>
+      <tr>
+        <td>Stems</td>
+        <td align="center"><input type="text" name="qtystems" size="3" maxlength="3"></td>
+      </tr>
+      <tr>
+        <td>Tops</td>
+        <td align="center"><input type="text" name="qtytops" size="3"
+        maxlength="3"></td>
+      </tr>
+      <tr>
+        <td colspan="2" align="center"><br>
+        <input type="submit" value="Enter Order"></td>
+      </tr>
+    </table>
+  </form>
+
 </body>
 
 </html>
---
+++ notindex.php
@@ -0,0 +1,12 @@
+<!DOCTYPE html>
+
+<head>
+<title>Antras puslapis</title>
+<meta content="text/html; charset=iso-8859-1" />
+<link rel="stylesheet" href="css.css" type="text/css" />
+</head>
+
+<body >
+
+</body>
+</html>
